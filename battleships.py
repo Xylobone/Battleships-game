5d505fd2477b3111187e77cc2998fb34bab832ba
Xylobone <lukemanntang@gmail.com>
2021-01-18 21:17:03 +0000

    Added additional functions and tested that they all worked. Additionally tested functions within the the application which now functions like properly but without any visualisation.

--- battleships.py
+++ battleships.py
@@ -1,24 +1,6 @@
 #see the readme.md file for description and data 
 
-
-'''
-class ship:
-    def __init__(self, row, column, horizontal, length, hits):
-        self.length = 0
-        self.row = 0
-        self.column = 0
-        self.horizontal = False
-        self.length = 0
-        self.hits = ()
-
-
-# (0row, 1column, 2horizontal, 3length, 4hits)
-
-
-'''
-
 import random
-fleet = []
 
 def is_sunk(ship):
     if ship[3] == len(ship[4]):
@@ -39,7 +21,7 @@ def ship_type(ship):
 
 def is_open_sea(row, column, fleet):
     # Iterates through each ship in fleet
-    for i in fleet: 
+    for i in range(len(fleet)): 
 
         # Creating variables to make it easier to code conditions
         # 'ship' variables refer to the values of the ship being compared to user row and column values
@@ -55,30 +37,30 @@ def is_open_sea(row, column, fleet):
 
         elif shiphorizontal == False:
             # Returns false if the row and column values are adjacent to the current ship
-            if (((shiprow - 1) <= row <= (shiprow + length + 1)) and ((column - 1) <= shipcolumn <= (column + 1))):
+            if (((shiprow - 1) <= row <= (shiprow + shiplength + 1)) and ((column - 1) <= shipcolumn <= (column + 1))):
                 return False
 
         else:
             # If it pases these conditions then the function will return that the square specified is in open sea.
             return True
-        
-
 
 def ok_to_place_ship_at(row, column, horizontal, length, fleet):
     
     # For loop to iterate through each ship in list (fleet) 
-    for i in fleet:
+    for i in range(len(fleet)):
         # if the ship is horizontal then we must check is_open_sea along column value (iterate through x axis)
         if horizontal == True:
             for j in range(0, length + 1):
                 if is_open_sea(row, column + j, fleet) == False:
                     return False
+            return True
         
         # if the ship is vertical then we must check is_open_sea along row value (iterate through y axis)
         elif horizontal == False:
             for k in range(0, length + 1):
                 if is_open_sea(row + k, column, fleet) == False:
                     return False
+            return True
         else:
             return True
             
@@ -87,141 +69,158 @@ def place_ship_at(row, column, horizontal, length, fleet):
 
     # Creating a new ship. 0 is a placeholder for set of tuples that will be the hits
     # Since we have to remake tuples whenever we edit them I thought this was a suitable solution
-    fleet.append((row, column, horizontal, length, 0))
+    fleet.append((row, column, horizontal, length, {}))
 
-
-def randomly_place_all_ships():
-
-    numberofships = 0
-
-    # Placing battleship
-    randomhorizontal = random.randint(0, 1)
-
-    if randomhorizontal == 1:
-        randomrow = random.randint(0, 9)
-        randomcolumn = random.randint(0, 6)
-        place_ship_at(randomrow, randomcolumn, True, 4, 0, fleet1)
-
-    elif randomhorizontal == 0:
-        randomrow = random.randint(0, 6)
-        randomcolumn = random.randint(0, 9)
-        place_ship_at(randomrow, randomcolumn, False, 4, 0, fleet1)
-    
-    numberofships = numberofships + 1
-    
-
-    # Placing Cruisers
-    while (1 < numberofships < 3):
-            
-        randomhorizontal = random.randint(0, 1)
-        
-        if randomhorizontal == 1:
-
-            randomrow = random.randint(0, 9)
-            randomcolumn = random.randint(0, 7)
-            if ok_to_place_ship_at(randomrow, randomcolumn, True, 3, fleet1) == True:
-                place_ship_at(randomrow, randomcolumn, True, 3, 0, fleet1)
-                numberofships = numberofships + 1
-
-        elif randomhorizontal == 0:
-            
-            randomrow = random.randint(0, 7)
-            randomcolumn = random.randint(0, 9)
-            if ok_to_place_ship_at(randomrow, randomcolumn, False, 3, fleet1) == True:
-                place_ship_at(randomrow, randomcolumn, False, 3, 0, fleet1)
-                numberofships = numberofships + 1
-
-    # Placing destroyers
-    while 3 <= numberofships < 6:
-
-        randomhorizontal = random.randint(0, 1)
-
-        if randomhorizontal == 1:
-
-            randomrow = random.randint(0, 9)
-            randomcolumn = random.randint(0, 8)
-            if ok_to_place_ship_at(randomrow, randomcolumn, True, 2, fleet1) == True:
-                place_ship_at(randomrow, randomcolumn, True, 2, 0, fleet1)
-                numberofships = numberofships + 1
-
-        elif randomhorizontal == 0:
-            
-            randomrow = random.randint(0, 8)
-            randomcolumn = random.randint(0, 9)
-            if ok_to_place_ship_at(randomrow, randomcolumn, False, 2, fleet1) == True:
-                place_ship_at(randomrow, randomcolumn, False, 2, 0, fleet1)
-                numberofships = numberofships + 1
-    
-    while numberofships < 10:
-        
-        randomhorizontal = random.randint(0, 1)
-
-        if randomhorizontal == 1:
-
-            randomrow = random.randint(0, 9)
-            randomcolumn = random.randint(0, 8)
-            if ok_to_place_ship_at(randomrow, randomcolumn, True, 1, fleet1) == True:
-                place_ship_at(randomrow, randomcolumn, True, 1, 0, fleet1)
-                numberofships = numberofships + 1
-
-        elif randomhorizontal == 0:
-            
-            randomrow = random.randint(0, 8)
-            randomcolumn = random.randint(0, 9)
-            if ok_to_place_ship_at(randomrow, randomcolumn, False, 1, fleet1) == True:
-                place_ship_at(randomrow, randomcolumn, False, 1, 0, fleet1)
-                numberofships = numberofships + 1
 # (0row, 1column, 2horizontal, 3length, 4hits)
 def check_if_hits(row, column, fleet):
     
-    for i in fleet:
+    for i in range(len(fleet)):
         
         shiplength = fleet[i][3]
         shiphorizontal = fleet[i][2]
         shiprow = fleet[i][0]
         shipcolumn = fleet[i][1]
-
-        if shiphorizontal == True:
-            if row != shiprow:
-                return False
-            elif ((shipcolumn <= column <= (shipcolumn + shiplength)):
-                return True
         
-        elif shiphorizontal == False:
-            if column != shiprow:
-                return False
-            elif ((shiprow) <= row <= (shiprow + shiplength)):
+        if shiphorizontal == True:
+            if (row == shiprow) and (shipcolumn <= column <= (shipcolumn + shiplength)):
                 return True
+
+        elif shiphorizontal == False:
+            if (column == shipcolumn) and (shiprow <= row <= (shiprow + shiplength)):
+                return True
+
+    # If the function reaches this part without return True then then it must not be a hit.
+    # This is because it would have exited the function already with a True return otherwise
+    return False
 
 def hit(row, column, fleet):
     
+    # Since ship is not specified in the function, it will
+    # search through fleet for the ship that is hit and then
+    # return the tuple containing the fleet and ship
+    for i in range(len(fleet)):
+        shiplength = fleet[i][3]
+        shiphorizontal = fleet[i][2]
+        shiprow = fleet[i][0]
+        shipcolumn = fleet[i][1]
+        shiphits = set(fleet[i][4])
+            
+        #Finding out which ship is hit
+        if shiphorizontal == True:
+            if (row == shiprow) and ((shipcolumn) <= column <= (shipcolumn + shiplength)):
+                shiphits.add((row, column))
+                fleet[i] = (shiprow, shipcolumn, True, shiplength, shiphits)
+                return (fleet, fleet[i])
+
+        
+        elif shiphorizontal == False:
+            if (column == shipcolumn) and ((shiprow) <= row <= (shiprow + shiplength)):
+                shiphits.add((row, column))
+                fleet[i] = (shiprow, shipcolumn, False, shiplength, shiphits)
+                return (fleet, fleet[i])
 
 def are_unsunk_ships_left(fleet):
 
     # 0 means all ships are sunk
     allshipssunk = 0
 
-    for i in fleet:
+    for i in range(len(fleet)):
         if is_sunk(fleet[i]) == False:
             # Will add 1 if it finds an unsunk ship
             allshipssunk = allshipssunk + 1
     
     # It requires all ships to be sunk for allshipssunk to be 0
-    if unsunkships == 0:
+    if allshipssunk == 0:
         return False 
     else:
         return True
 
-
-            
-
-
-
 def main():
-    #the implementation provided below is indicative only
-    #you should improve it or fully rewrite to provide better functionality (see readme file)
-    current_fleet = randomly_place_all_ships()
 
+    fleet1 = []
+    def randomly_place_all_ships():
+
+        numberofships = 0
+        # Placing battleship
+        randomhorizontal = random.randint(0, 1)
+
+        if randomhorizontal == 1:
+            randomrow = random.randint(0, 9)
+            randomcolumn = random.randint(0, 6)
+            place_ship_at(randomrow, randomcolumn, True, 4, fleet1)
+
+        elif randomhorizontal == 0:
+            randomrow = random.randint(0, 6)
+            randomcolumn = random.randint(0, 9)
+            place_ship_at(randomrow, randomcolumn, False, 4, fleet1)
+        
+        numberofships = numberofships + 1
+
+        # Placing Cruisers
+        while (1 <= numberofships < 3):
+                
+            randomhorizontal = random.randint(0, 1)
+            
+            if randomhorizontal == 1:
+
+                randomrow = random.randint(0, 9)
+                randomcolumn = random.randint(0, 7)
+                if ok_to_place_ship_at(randomrow, randomcolumn, True, 3, fleet1) == True:
+                    place_ship_at(randomrow, randomcolumn, True, 3, fleet1)
+                    numberofships = numberofships + 1
+
+            elif randomhorizontal == 0:
+                
+                randomrow = random.randint(0, 7)
+                randomcolumn = random.randint(0, 9)
+                if ok_to_place_ship_at(randomrow, randomcolumn, False, 3, fleet1) == True:
+                    place_ship_at(randomrow, randomcolumn, False, 3, fleet1)
+                    numberofships = numberofships + 1
+
+        # Placing destroyers
+        while 3 <= numberofships < 6:
+
+            randomhorizontal = random.randint(0, 1)
+
+            if randomhorizontal == 1:
+
+                randomrow = random.randint(0, 9)
+                randomcolumn = random.randint(0, 8)
+                if ok_to_place_ship_at(randomrow, randomcolumn, True, 2, fleet1) == True:
+                    place_ship_at(randomrow, randomcolumn, True, 2, fleet1)
+                    numberofships = numberofships + 1
+
+            elif randomhorizontal == 0:
+                
+                randomrow = random.randint(0, 8)
+                randomcolumn = random.randint(0, 9)
+                if ok_to_place_ship_at(randomrow, randomcolumn, False, 2, fleet1) == True:
+                    place_ship_at(randomrow, randomcolumn, False, 2, fleet1)
+                    numberofships = numberofships + 1
+        
+        # Placing Submarines
+        while 6 <= numberofships < 10:
+            
+            randomhorizontal = random.randint(0, 1)
+
+            if randomhorizontal == 1:
+
+                randomrow = random.randint(0, 9)
+                randomcolumn = random.randint(0, 9)
+                if ok_to_place_ship_at(randomrow, randomcolumn, True, 1, fleet1) == True:
+                    place_ship_at(randomrow, randomcolumn, True, 1, fleet1)
+                    numberofships = numberofships + 1
+
+            elif randomhorizontal == 0:
+                
+                randomrow = random.randint(0, 9)
+                randomcolumn = random.randint(0, 9)
+                if ok_to_place_ship_at(randomrow, randomcolumn, False, 1, fleet1) == True:
+                    place_ship_at(randomrow, randomcolumn, False, 1, fleet1)
+                    numberofships = numberofships + 1
+
+    randomly_place_all_ships()
+    
     game_over = False
     shots = 0
 
@@ -230,15 +229,15 @@ def main():
         current_row = int(loc_str[0])
         current_column = int(loc_str[1])
         shots += 1
-        if check_if_hits(current_row, current_column, current_fleet):
+        if check_if_hits(current_row, current_column, fleet1) == True:
             print("You have a hit!")
-            (current_fleet, ship_hit) = hit(current_row, current_column, current_fleet)
+            (fleet1, ship_hit) = hit(current_row, current_column, fleet1)
             if is_sunk(ship_hit):
                 print("You sank a " + ship_type(ship_hit) + "!")
         else:
             print("You missed!")
 
-        if not are_unsunk_shis_left(current_fleet): game_over = True
+        if not are_unsunk_ships_left(fleet1): game_over = True
 
     print("Game over! You required", shots, "shots.")
 
--- test_battleships.py
+++ test_battleships.py
@@ -1,10 +1,12 @@
 import pytest
+import random
 from battleships import *
+
 
 def test_is_sunk1():
     ship = (2, 3, False, 3, {(2,3), (3,3), (4,3)})
     assert is_sunk(ship) == True
-    #add at least four more tests for is_sunk by the project submission deadline
+    
 
 def test_ship_type1():
     ship = (2, 3, False, 3, {(2,3), (3,3), (4,3)})
@@ -13,58 +15,78 @@ def test_ship_type1():
 def test_ship_type2():
     ship = (2, 3, False, 4, {(2,3), (3,3), (4,3)})
     assert ship_type(ship) == "battleship"
-    #add at least one test for ship_type by the deadline of session 7 assignment
-    #provide at least five tests in total for ship_type by the project submission deadline
+
 
 def test_is_open_sea1():
     ship = (2, 3, False, 4, {(2,3), (3,3), (4,3)})
     fleet1 = [ship]
-    result = is_open_sea(1, 2, fleet1)
-    assert result == True
-    #add at least one test for open_sea by the deadline of session 7 assignment
-    #provide at least five tests in total for open_sea by the project submission deadline
+    result = is_open_sea(1, 2, fleet1)    
+    assert result == False
+
 
 def test_ok_to_place_ship_at1():
     #(row, column, horizontal, length, fleet)
-    ship = (2, 3, False, 4, {(2,3), (3,3), (4,3)})
+    ship = (3, 3, False, 4, {(2,3), (3,3), (4,3)})
     ship2 = (6, 7, False, 3, {(6,6), (6,5)})
     fleet1 = [ship, ship2]
-    result = ok_to_place_ship_at(3, 5, True, 3, fleet1)
-    assert result == True
-    #add at least one test for ok_to_place_ship_at by the deadline of session 7 assignment
-    #provide at least five tests in total for ok_to_place_ship_at by the project submission deadline
+    assert ok_to_place_ship_at(1, 1, True, 1, fleet1) == True
+
+def test_ok_to_place_ship_at2():
+    #(row, column, horizontal, length, fleet)
+    ship = (3, 3, False, 4, {(2,3), (3,3), (4,3)})
+    ship2 = (6, 7, False, 3, {(6,6), (6,5)})
+    fleet1 = [ship, ship2]
+    assert ok_to_place_ship_at(3, 3, True, 1, fleet1) == False
 
 def test_place_ship_at1():
     #(row, column, horizontal, length, fleet)
     ship = (2, 3, False, 4, {(2,3), (3,3), (4,3)})
     ship2 = (6, 7, False, 3, {(6,6), (6,5)})
     fleet1 = [ship, ship2]
-    assert len(place_ship_at(1, 9, True, 2, fleet1)) == 3
-    #add at least one test for place_ship_at by the deadline of session 7 assignment
-    #provide at least five tests in total for place_ship_at by the project submission deadline
+    place_ship_at(1, 9, True, 2, fleet1)
+    print(fleet1)
+    assert fleet1[2] == (1, 9, True, 2, {})
+
+def test_place_ship_at2():
+    #(row, column, horizontal, length, fleet)
+    fleet1 = []
+    place_ship_at(8, 8, True, 2, fleet1)
+    print(fleet1)
+    assert fleet1[0] == (8, 8, True, 2, {})
+
+def test_randomly_place_all_ships():
+    # Printing fleet and length of fleet to check the contents of the fleet during testing
+    randomly_place_all_ships()
+    print(fleet1)
+    print(len(fleet1))
+    assert len(fleet1) == 10
 
 def test_check_if_hits1():
     #(row, column, fleet)
-    fleet1 = [ship, ship2]
-    ship2 = (6, 7, False, 3, {(6,6), (6,5)})
-    check_if_hits(6,7, fleet1)
-    assert check_if_hits(6,7, fleet1)== True
-    #add at least one test for check_if_hits by the deadline of session 7 assignment
-    #provide at least five tests in total for check_if_hits by the project submission deadline
+    ship = (2, 3, False, 4, {(2,3), (3,3), (4,3)})
+    ship2 = (6, 7, False, 3, {(7,7), (8,7)})
+    ship3 = (1, 1, True, 1)
+    fleet1 = [ship, ship2, ship3]
+    check_if_hits(1,1, fleet1)
+    assert check_if_hits(1, 1, fleet1) == True
 
 def test_hit1():
     #(row, column, fleet)
-    ship = (2, 3, False, 4, {(2,3), (3,3)})
+    ship = (2, 3, False, 4, {(3,3)})
     fleet1 = [ship]
-    shiphit = hit(4, 3, fleet1)
-    assert shiphit == (ship, fleet1)
-    #add at least one test for hit by the deadline of session 7 assignment
-    #provide at least five tests in total for hit by the project submission deadline
+    shiphit = hit(2, 3, fleet1)
+    assert shiphit == (fleet1, (2, 3, False, 4, {(2,3), (3,3)}))
+
+def test_hit2():
+    #(row, column, fleet)
+    ship = (2, 3, False, 4, {})
+    fleet1 = [ship]
+    shiphit = hit(2, 3, fleet1)
+    assert shiphit == (fleet1, (2, 3, False, 4, {(2,3)}))
 
 def test_are_unsunk_ships_left1():
     ship = (2, 3, False, 4, {(2,3), (3,3), (4,3)})
-    ship2 = (6, 7, False, 3, {(6,6), (6,5)})
+    ship2 = (6, 7, False, 3, {(6, 7), (7, 7)})
+    ship3 = (9, 9, True, 1, {(9, 9)})
     fleet1 = [ship, ship2]
-    assert are_unsunk_ships_left == True
-    #add at least one test for are_unsunk_ships_left by the deadline of session 7 assignment
-    #provide at least five tests in total for are_unsunk_ships_left by the project submission deadline
+    assert are_unsunk_ships_left(fleet1) == True
